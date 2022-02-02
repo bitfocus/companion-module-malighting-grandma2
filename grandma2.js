2583a311bdadac681123c27f45a57ad539a977ce
Julian Waller <git@julusian.co.uk>
2022-02-02 16:43:42 +0000

    bulk replace system.emit calls

--- grandma2.js
+++ grandma2.js
@@ -285,7 +285,7 @@ instance.prototype.destroy = function() {
 
 instance.prototype.actions = function(system) {
 	var self = this;
-	self.system.emit('instance_actions', self.id, {
+	self.setActions({
 
 		'command': {
 			label:'Run Command',
